b493c48d62e61aa5a25886d7ebd07026eceb6f09
Mervan Yalçın <mervan37ylcn@gmail.com>
2022-10-30 01:08:03 +0300

    Added Direct Message

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import {useField} from "formik";
 import classNames from "classnames";
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import {initializeApp} from "firebase/app";
+import { initializeApp } from "firebase/app";
 import {
-	createUserWithEmailAndPassword,
-	getAuth,
-	onAuthStateChanged,
-	signInWithEmailAndPassword,
-	signOut,
-	updateProfile
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
 } from "firebase/auth";
-import {doc, getDoc, getFirestore, setDoc} from "firebase/firestore"
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 import toast from "react-hot-toast";
-import {userHandle} from "utils";
+import { userHandle } from "utils";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBw-E8teeIeOwfKHKTNg0H-NC6io1tJXqs",
@@ -22,71 +22,87 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth()
-const db = getFirestore(app)
+const auth = getAuth();
+const db = getFirestore(app);
 
-onAuthStateChanged(auth, async user => {
-	if (user) {
-		const dbUser = await getDoc(doc(db, "users", user.uid))
-		let data = {
-			uid: user.uid,
-			fullName: user.displayName,
-			email: user.email,
-			emailVerified: user.emailVerified,
-			...dbUser.data()
-		}
-		userHandle(data)
-	} else {
-		userHandle(false)
-	}
-})
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const dbUser = await getDoc(doc(db, "users", user.uid));
+    let data = {
+      uid: user.uid,
+      fullName: user.displayName,
+      email: user.email,
+      emailVerified: user.emailVerified,
+      ...dbUser.data(),
+    };
+    userHandle(data);
+  } else {
+    userHandle(false);
+  }
+});
 
 export const login = async (email, password) => {
-	try {
-		return await signInWithEmailAndPassword(auth, email, password)
-	} catch (err) {
-		toast.error(err.code)
-	}
-}
+  try {
+    return await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    toast.error(err.code);
+  }
+};
 
-export const register = async ({email, password, full_name, username}) => {
-	try {
-		const user = await getDoc(doc(db, "usernames", username))
-		if (user.exists()) {
-			toast.error(`${username} kullanıcı adı başkası tarafından kullanılıyor.`)
-		} else {
-			const response = await createUserWithEmailAndPassword(auth, email, password)
-			if (response.user) {
+export const getUserInfo = async (uname) => {
+  const username = await getDoc(doc(db, "usernames", uname));
+  if (username.exists()) {
+    return await (await getDoc(doc(db, "users", username.data().user_id))).data();
+  } else {
+	toast.error("Kullanıcı bulunamadı")
+    throw new Error("Kullanıcı bulunamadı")
+  }
+};
 
-				await setDoc(doc(db, "usernames", username), {
-					user_id: response.user.uid
-				})
+export const register = async ({ email, password, full_name, username }) => {
+  try {
+    const user = await getDoc(doc(db, "usernames", username));
+    if (user.exists()) {
+      toast.error(`${username} kullanıcı adı başkası tarafından kullanılıyor.`);
+    } else {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (response.user) {
+        await setDoc(doc(db, "usernames", username), {
+          user_id: response.user.uid,
+        });
 
-				await setDoc(doc(db, "users", response.user.uid), {
-					fullName: full_name,
-					username: username,
-					followers: [],
-					following: [],
-					notifications: []
-				})
+        await setDoc(doc(db, "users", response.user.uid), {
+          fullName: full_name,
+          username: username,
+          followers: [],
+          following: [],
+          notifications: [],
+          website: "",
+          bio: "",
+          phoneNumber: "",
+          post: 0,
+        });
 
-				await updateProfile(auth.currentUser, {
-					displayName: full_name
-				})
+        await updateProfile(auth.currentUser, {
+          displayName: full_name,
+        });
 
-				return response.user
-
-			}
-		}
-	} catch (err) {
-		toast.error(err.code)
-	}
-}
+        return response.user;
+      }
+    }
+  } catch (err) {
+    toast.error(err.code);
+  }
+};
 
 export const logout = async () => {
-	try {
-		await signOut(auth)
-	} catch (err) {
-		toast.error(err.code)
-	}
-}
+  try {
+    await signOut(auth);
+  } catch (err) {
+    toast.error(err.code);
+  }
+};
